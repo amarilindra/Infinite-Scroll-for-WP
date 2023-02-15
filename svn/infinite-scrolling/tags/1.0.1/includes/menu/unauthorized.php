d6dea4f8374e80797c75e6059c2cf56bb760262c
Amar Ilindra <geekdashboard@gmail.com>
2023-02-15 22:12:38 +0530

    Stable Tag Updated

--- svn/infinite-scrolling/tags/1.0.1/includes/menu/unauthorized.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-function ikva_infinite_scroll_for_wp_unauthorized() {
-    echo '<div class="notice-error notice"><p><strong> ✋ Stop: You are not allowed to access this tab 👻</strong></p></div><style>.ikva-infinite-scroll-tabs{display: none}</style>';
-}
